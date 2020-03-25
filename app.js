be1b827ff08b93fc9f96588061d7d8fa16554c9b
yugiyns <33834527+yugiyns@users.noreply.github.com>
2020-03-25 18:52:01 +0300

    node.js-map

---
+++ app.js
@@ -0,0 +1,14 @@
+var http = require('http');
+var fs = require ('fs');
+
+
+var server = http.createServer(function(reg,res){
+    console.log('request was made :' + reg.url);
+    res.writeHead(200,{'Content-Type': 'text/html'});
+    var myReadStream = fs.createReadStream(__dirname + '/login.html', 'utf8');
+    myReadStream.pipe(res);
+});
+
+
+server.listen(3000, '127.0.0.1');
+console.log('yo dawgs,now listening to port 3000');
